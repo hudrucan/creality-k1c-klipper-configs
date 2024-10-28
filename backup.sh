0c6ce915d69eb6f058395986cfce77d387548516
hudrucan <hudrucan@gmail.com>
2024-10-28 18:27:45 +0700

    New backup from 10/28/24 - 18:27:45

---
+++ backup.sh
@@ -0,0 +1,14 @@
+HOME_DIR="/usr/data"
+BACKUP_DIR="$HOME_DIR/k1c-config"
+
+[ -e $BACKUP_DIR/printer_configs ] && rm -rf $BACKUP_DIR/printer_configs
+[ -e $BACKUP_DIR/guppyconfig.json ]  && rm $BACKUP_DIR/guppyconfig.json
+
+cp $HOME_DIR/guppyscreen/guppyconfig.json $BACKUP_DIR/guppyconfig.json
+cp -r $HOME_DIR/printer_data/config $BACKUP_DIR/printer_configs
+cp -r $HOME_DIR/printer_data/config $BACKUP_DIR/printer_configs
+
+git add .
+git commit -m "New backup from $(date +"%x - %X")"
+git push origin HEAD
+
